10773aaef6a77a93b76cfb23b7430cb07433f288
Hugkinkazu <k1.kinjo092@gmail.com>
2023-09-26 15:16:23 +0900

    webサイトの簡易作成

---
+++ IndoorStylistApp/views.py
@@ -0,0 +1,4 @@
+from IndoorStylistApp import app
+@app.route('/')
+def index():
+    return 'Hellow World!'
---
+++ server.py
@@ -0,0 +1,3 @@
+from IndoorStylistApp import app
+if __name__ == '__main__':
+    app.run(debug=True)
