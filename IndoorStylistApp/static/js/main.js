0dd7ad4c69358862364c7792454607bc87a0a79a
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2023-09-28 14:00:43 +0900

    衣装の切り替えができるようにした

---
+++ IndoorStylistApp/static/js/main.js
@@ -0,0 +1,11 @@
+document.getElementById('btn3').onclick = function() {
+    console.log("Change Clothes");
+    let src = "/static/images/yu.png"
+    document.getElementById("pics").src=src;
+}
+
+document.getElementById('btn4').onclick = function() {
+    console.log("Change Clothes");
+    let src = "/static/images/m_clothes.png"
+    document.getElementById("pics").src=src;
+}
